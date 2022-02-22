79803b5e02e33e91263b33876013e000c20a5397
davidanthonyn <55152980+davidanthonyn@users.noreply.github.com>
2022-02-22 22:15:13 +0700

    Update Product.php

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Productadmin extends CI_Controller
+class Product extends CI_Controller
 {
 
     function __construct()
