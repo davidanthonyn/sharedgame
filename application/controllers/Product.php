5fc4e541cf9374ea238c231126550715afd2051a
davidanthonyn <55152980+davidanthonyn@users.noreply.github.com>
2022-03-14 16:23:27 +0700

    Update Product.php

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -17,7 +17,7 @@ class Product extends CI_Controller
     {
         $this->load->database();
         $this->load->model('Modelproduk');
-        $data['data'] = $this->Modelproduk->GetProduk();
+        $data["data"] = $this->Modelproduk->GetProduk();
         $data['title'] = 'Products | SharedGame';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         $this->load->view('includes/header.php', $data);
