aa893404eede78997565b861f5c3aee4b4b2ce9c
davidanthonyn <55152980+davidanthonyn@users.noreply.github.com>
2022-03-25 21:50:03 +0700

    Update footer.php

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -26,7 +26,7 @@
               <button type="submit" name="submit" id="submit" class="btn btn-block">Subscribe <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></button>
             </form>
             <p class="subscribed-text">*We send great deals and latest auto news to our subscribed users very period.</p>
-            <?php echo form_error('email', '<h6>', '</h6>');
+            <?php //echo form_error('email', '<h6>', '</h6>');
             ?>
           </div>
         </div>
